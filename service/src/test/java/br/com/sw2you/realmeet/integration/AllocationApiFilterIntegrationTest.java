fd6215ae7c3568a2ffcc8e27843cc72140f87e24
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-05-12 23:20:26 -0300

    test cases for allocation pagination

--- service/src/test/java/br/com/sw2you/realmeet/integration/AllocationApiFilterIntegrationTest.java
+++ service/src/test/java/br/com/sw2you/realmeet/integration/AllocationApiFilterIntegrationTest.java
@@ -10,17 +10,31 @@ import static br.com.sw2you.realmeet.utils.TestConstants.DEFAULT_ROOM_NAME;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.allocationBuilder;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.newEmployee;
 import static br.com.sw2you.realmeet.utils.TestDataCreator.roomBuilder;
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import br.com.sw2you.realmeet.api.facade.AllocationApi;
 import br.com.sw2you.realmeet.core.BaseIntegrationTest;
+import br.com.sw2you.realmeet.domain.entity.Allocation;
 import br.com.sw2you.realmeet.domain.repository.AllocationRepository;
 import br.com.sw2you.realmeet.domain.repository.RoomRepository;
+import br.com.sw2you.realmeet.service.AllocationService;
+import br.com.sw2you.realmeet.utils.TestDataCreator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.HttpClientErrorException;
 
 class AllocationApiFilterIntegrationTest extends BaseIntegrationTest {
+    @Autowired
+    private AllocationService allocationService;
+
     @Autowired
     private AllocationApi api;
 
@@ -137,5 +151,67 @@ class AllocationApiFilterIntegrationTest extends BaseIntegrationTest {
         );
 
         assertFalse(result.isEmpty());
+    }
+
+    @Test
+    void testFilterAllocationUsingPagination() {
+        persistAllocations(15);
+        ReflectionTestUtils.setField(allocationService, "maxLimit", 10);
+
+        var page1Result = api.listAllocations(null, null, null, null, null, null, 0);
+        var page2Result = api.listAllocations(null, null, null, null, null, null, 1);
+        assertEquals(10, page1Result.size());
+        assertEquals(5, page2Result.size());
+    }
+
+    @Test
+    void testFilterAllocationUsingPaginationAndLimit() {
+        persistAllocations(25);
+        ReflectionTestUtils.setField(allocationService, "maxLimit", 50);
+
+        var page1Result = api.listAllocations(null, null, null, null, null, 10, 0);
+        var page2Result = api.listAllocations(null, null, null, null, null, 10, 1);
+        var page3Result = api.listAllocations(null, null, null, null, null, 10, 2);
+        assertEquals(10, page1Result.size());
+        assertEquals(10, page2Result.size());
+        assertEquals(5, page3Result.size());
+    }
+
+    @Test
+    void testFilterAllocationOrderByStartAtDesc() {
+        var allocations = persistAllocations(3);
+
+        var result = api.listAllocations(null, null, null, null, "-startAt", null, null);
+
+        assertEquals(3, result.size());
+        assertEquals(allocations.get(0).getId(), result.get(2).getId());
+        assertEquals(allocations.get(1).getId(), result.get(1).getId());
+        assertEquals(allocations.get(2).getId(), result.get(0).getId());
+    }
+
+    @Test
+    void testFilterAllocationOrderByInvalidField() {
+        assertThrows(
+            HttpClientErrorException.UnprocessableEntity.class,
+            () -> api.listAllocations(null, null, null, null, "invalid", null, null)
+        );
+    }
+
+    private List<Allocation> persistAllocations(int numberOfAllocations) {
+        var room = roomRepository.saveAndFlush(roomBuilder().build());
+
+        return IntStream
+            .range(0, numberOfAllocations)
+            .mapToObj(
+                i ->
+                    allocationRepository.saveAndFlush(
+                        allocationBuilder(room)
+                            .subject(DEFAULT_ALLOCATION_SUBJECT + "_" + (i + 1))
+                            .startAt(DEFAULT_ALLOCATION_START_AT.plusHours(i + 1))
+                            .endAt(DEFAULT_ALLOCATION_END_AT.plusHours(i + 1))
+                            .build()
+                    )
+            )
+            .collect(toList());
     }
 }
